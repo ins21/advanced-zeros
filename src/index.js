02c3d40b35c023966a4c58e9f3c7147976883b2c
ins21 <leonnby@gmail.com>
2019-03-22 01:47:30 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let baseFactors = {};
+  let min = 0;
+
+  for (let i = 2; i <= base; i++) {
+    while (base % i === 0) {
+      baseFactors[i] ? baseFactors[i]++ : baseFactors[i] = 1;
+      base = base / i;
+    }
+  }
+
+  for (let key in baseFactors) {    
+    let sum = 0;
+    let n = number;
+
+    while (n > 1) {
+      sum = sum + Math.floor(n / key);
+      n = Math.floor(n / key);
+    }
+    
+    if (min === 0 || min >= sum ) min = Math.floor(sum / baseFactors[key]);
+  }
+  
+  return min;
 }
